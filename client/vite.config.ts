c8c453b96ac52c9665d233015b522052788eeceb
Sara Chester <sara3280624@gmail.com>
2025-10-19 10:36:49 +0300

    _

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+  },
+  server: {
+    port: 5173,
+    strictPort: false,
+  }
 })
