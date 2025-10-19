2917754650e0a94d792db01877688f1706fe0634
Sara Chester <sara3280624@gmail.com>
2025-10-19 15:19:17 +0300

    _

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const __filename = __filename;
+const __dirname = __dirname;
 
 const app = express();
 const dbPath = path.join(__dirname, 'db.json');
